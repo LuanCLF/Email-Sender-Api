4da76a496b8b5f84aade3bb909aae5bee0c564b3
Luan Charlys <firminocharlys@gmail.com>
2023-10-29 23:06:09 -0300

    feat: conexao e versao 1.0 da api

--- src/connection/connection.ts
+++ src/connection/connection.ts
@@ -1,14 +1,18 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+import { Transporter } from 'nodemailer';
 
-const nodemailer = require('nodemailer');
+dotenv.config();
 
-const transportador = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+console.log(process.env.EMAIL_PASS);
+
+const sender: Transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST as string,
+  port: Number(process.env.EMAIL_PORT),
   auth: {
-    user: process.env.EMAIL_LOGIN,
-    pass: process.env.EMAIL_SENHA,
+    user: process.env.EMAIL_LOGIN as string,
+    pass: process.env.EMAIL_PASS as string,
   },
 });
 
-module.exports = transportador;
+export default sender;
